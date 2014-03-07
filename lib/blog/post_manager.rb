c9784df82f7ea72c6ad7d0bf164835dae8999dd6
Erick C <ecastillo@pernix-solutions.com>
2014-03-07 11:48:19 -0600

    REFACTOR: Save action edit post

--- lib/blog/post_manager.rb
+++ lib/blog/post_manager.rb
@@ -19,14 +19,27 @@ module Blog
     end
 
     def self.save(params)
-      id = params[:id]
-      if id == '' || id == nil then DBAdapter.new_post(params[:title], params[:body])
-      else DBAdapter.edit_post(title: params[:title], body: params[:body])
+      if no_id?(params[:id]) then new_post(params)
+      else edit_post(params)
       end
     end
 
     def self.save_action?(params)
       params[:action] == SAVE
+    end
+
+    def self.new_post(params)
+      DBAdapter.new_post(params[:title], params[:body])
+    end
+
+    def self.edit_post(params)
+      title = params[:title]
+      body = params[:body]
+      DBAdapter.edit_post(title: title, body: body)
+    end
+
+    def self.no_id?(id)
+      id == '' || id == nil
     end
   end
 end
