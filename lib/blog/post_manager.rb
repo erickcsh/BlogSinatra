aa8a206255d28189da456537c85beb48bdd7c313
Erick C <ecastillo@pernix-solutions.com>
2014-03-07 11:36:05 -0600

    REFACTOR: Save action create new post

--- lib/blog/post_manager.rb
+++ lib/blog/post_manager.rb
@@ -5,7 +5,7 @@ module Blog
     SAVE = 'Save'
 
     def self.manage_post(params)
-      DBAdapter.new_post(params[:title], params[:body]) if params[:action] == SAVE
+      save(params) if save_action?(params)
       delete(params) if delete_action?(params)
     end
 
@@ -16,6 +16,14 @@ module Blog
 
     def self.delete_action?(params)
       params[:action] == DELETE
+    end
+
+    def self.save(params)
+      DBAdapter.new_post(params[:title], params[:body])
+    end
+
+    def self.save_action?(params)
+      params[:action] == SAVE
     end
   end
 end
