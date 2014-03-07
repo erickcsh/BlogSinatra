062789417f42e79b689615ba31d86de041e462ca
Erick C <ecastillo@pernix-solutions.com>
2014-03-07 12:45:58 -0600

    PASS: display instructions sidebar

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 require_relative 'lib/blog'
 
 get "/" do
+  erb :index
+end
+
+def posts_html
   erb :instructions_post
 end
 
+def sidebar_html
+  erb :instructions_sidebar
+end
